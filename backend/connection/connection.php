ff34d2318c25b5e0d095107474f2c2d3308c540a
santana <101341640+santaninhaaaa@users.noreply.github.com>
2024-03-19 08:36:17 -0300

    Add files via upload

---
+++ backend/connection/connection.php
@@ -0,0 +1,21 @@
+<?php
+
+$hostname = 'localhost';
+$dbname = 'biblioteca';
+$username = 'root';
+$password = 'usbw';
+
+try{
+    $pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $return = array(
+        'type' => 'success',
+        'message' => 'Conexão realizada com sucesso!'
+    );
+} catch (PDOException $e) {
+    $return = array(
+        'type' => 'error',
+        'message' => 'Erro: '.$e->getMessage()
+    );
+}
+echo json_encode($return);
